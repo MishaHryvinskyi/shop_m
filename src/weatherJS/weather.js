324f590795cb91e45be7410c8f63bcec5b4ce588
Misha <hryvinskyim@gmail.com>
2024-01-14 16:48:23 +0200

    weather

--- src/weatherJS/weather.js
+++ src/weatherJS/weather.js
@@ -8,7 +8,13 @@ function getWeather(city, days) {
 
     const BASE_URL = 'https://api.weatherapi.com/v1';
     const API_KEY = 'c0dca8ed73ba444e8db231919241301';
-    return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`)
+    // const params = new URLSearchParams({
+    //     key: API_KEY,
+    //     q: city,
+    //     days: days,
+    //     lang: "uk"
+    // })
+    return fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`)
     .then(response => {
         if(!response.ok) {
             throw new Error(response.statusText);
@@ -32,11 +38,11 @@ function onSearch(event) {
 function createMarkup(arr) {
 
     return arr.map(({ date, day:{ avgtemp_c, condition: { icon, text } }, }) => `
-        <li>
+        <li class="list__item-weather">
             <img src="${icon}" alt="${text}" />
             <p>${text}</p>
             <h2>${date}</h2>
-            <h3>${avgtemp_c}</h3>
+            <h3>${avgtemp_c} ℃</h3>
         </li>
     `).join('');
     list
