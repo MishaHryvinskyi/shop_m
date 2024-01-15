1851a31a999c9cfe5c0a2352628d3594c74e8a26
Misha <hryvinskyim@gmail.com>
2024-01-15 16:13:58 +0200

    fetch

--- src/newsSearch/markup.js
+++ src/newsSearch/markup.js
@@ -1,13 +1,19 @@
-function createMarkup(arr) {
-    return arr.map(({ author, title, description, url, urlToImage }) => `
+function createMarkup({ 
+    author, 
+    title, 
+    description, 
+    url, 
+    urlToImage 
+}) {
+    return  `
     <li class="js-item-search">
-        <img src="${urlToImage}" alt="${title}" width="550" />
         <h2>${title}</h2>
-        <h3>${author}</h3>
+        <h3>${author || "No author!😕"}</h3>
+        <img src="${urlToImage || "https://ornament.name/pic/no_result.jpg"}" alt="${title}" width="550" />
         <p>${description}</p>
         <a class="btn-s" href="${url}" target="_blank" rel="noreferrer noopener">Read more...</a>
     </li>
-`).join('');
+`;
 };
 
 export { createMarkup };
--- src/newsSearch/mewsSearch.js
+++ src/newsSearch/mewsSearch.js
@@ -8,12 +8,23 @@ function onSearch(event) {
     event.preventDefault();
     const form = event.currentTarget;
     const { query } = form.elements;
-    getNews(query.value).then(data => {
-        refs.listNews.innerHTML = createMarkup(data.articles)
+    getNews(query.value)
+    .then(({ articles }) => {
+        // refs.listNews.innerHTML = createMarkup(data.articles)
+        if(articles.length === 0) throw new Error("No data!");
+
+        const markup = articles.reduce((markup, articles) => markup + createMarkup(articles), "");
+        updateNewsList(markup);
     })
-    .catch(onError).finally(() => form.reset());
+    .catch(onError)
+    .finally(() => form.reset());
 }
 
 function onError(err) {
     console.error(err)
+    updateNewsList("<p>Not found!</p>")
+}
+
+function updateNewsList(markup) {
+    refs.listNews.innerHTML = markup
 }
