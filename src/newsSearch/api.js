c6d15b91a0477fc634278d36db8f6f59d4eb4f40
Misha <hryvinskyim@gmail.com>
2024-01-16 20:28:18 +0200

    comment query

--- src/newsSearch/api.js
+++ src/newsSearch/api.js
@@ -5,9 +5,9 @@ const API_KEY = "8ece45ab03a7434e9e4dffc54874f777";
 function getNews(query) {
     return fetch(`${BASE_URL}?apiKey=${API_KEY}&q=${query}&pageSize=5`)
     .then(res =>{
-        if(!res.ok) {
-            throw new Error(res.statusText);
-        }
+        // if(!res.ok) {
+        //     throw new Error(res.statusText);
+        // }
         return res.json();
     })
    
