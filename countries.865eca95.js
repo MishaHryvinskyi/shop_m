f1922523605bb8e33f34cf243f3753a45af4e670
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2024-01-19 13:25:55 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/shop_m@53808a064fa406523398d6b29f1d034c88135787 🚀

--- countries.865eca95.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".me");(async function(){try{const e="https://restcountries.com/v3.1/name/",t=["Ukraine","France","Germany"].map((async t=>(await fetch(`${e}${t}`)).json()));return await Promise.allSettled(t)}catch(e){console.log(e)}})().then((e=>console.log(e))).catch((e=>console.log(e)));
-//# sourceMappingURL=countries.865eca95.js.map
